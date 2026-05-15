2a506fa15f8ada180a5c2aeb4c5f71314c9b319f
Dipanshu Sharma <sharmadipanshu1904@gmail.com>
2026-05-16 04:58:48 +0530

    Remove parkease-angular from GitHub

--- parkease-angular/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: '',
-  // Replace with your Razorpay test key from https://dashboard.razorpay.com
-  razorpayKey: 'rzp_test_Spkl2xe5aisNaK'
-};
